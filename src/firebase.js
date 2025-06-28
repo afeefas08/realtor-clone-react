1f950980686501d95070c76a42fb40fff63e76ab
Afeefa <afeefas008@gmail.com>
2025-06-28 21:22:14 +0530

    install firebase and react toastify and sign up the user

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+const apiKeys = import.meta.env.VITE_GOOGLE_API_KEY;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAuxH712dEZAuOeulnfFPYFx98L_YJf3Po",
+
+  apiKey: apiKeys,
   authDomain: "realtor-clone-fe872.firebaseapp.com",
   projectId: "realtor-clone-fe872",
-  storageBucket: "realtor-clone-fe872.firebasestorage.app",
+  storageBucket: "realtor-clone-fe872.appspot.com",
   messagingSenderId: "796844449334",
   appId: "1:796844449334:web:d907f19ef23355b4058297"
 };
